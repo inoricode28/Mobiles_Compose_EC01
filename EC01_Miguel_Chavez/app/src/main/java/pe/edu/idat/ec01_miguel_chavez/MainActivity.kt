de317629f5c9a9386952a437471814f008acba9e
inoricode28 <inori237@gmail.com>
2024-06-09 09:41:54 -0500

    Initial

---
+++ EC01_Miguel_Chavez/app/src/main/java/pe/edu/idat/ec01_miguel_chavez/MainActivity.kt
@@ -0,0 +1,47 @@
+package pe.edu.idat.ec01_miguel_chavez
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.idat.ec01_miguel_chavez.ui.theme.EC01_Miguel_ChavezTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EC01_Miguel_ChavezTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EC01_Miguel_ChavezTheme {
+        Greeting("Android")
+    }
+}
